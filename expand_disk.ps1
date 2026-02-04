0fcd151db6d0b3aa35d1831902481bceca3b8696
caslacapo <53279675+caslacapo@users.noreply.github.com>
2026-02-04 17:35:47 -0500

    Update expand_disk.ps1

--- expand_disk.ps1
+++ expand_disk.ps1
@@ -1,18 +1,9 @@
-﻿param(
-    [Parameter(Mandatory=$true)]
-    [string]$DriveLetter
-)
+$DriveLetter = "C"
 
-# Ensure the drive letter is a single character without colon
-$DriveLetter = $DriveLetter.TrimEnd(':').ToUpper()
-
-# Get the partition
 $partition = Get-Partition -DriveLetter $DriveLetter
 
-# Get the maximum supported size
 $supportedSize = Get-PartitionSupportedSize -PartitionNumber $partition.PartitionNumber -DiskNumber $partition.DiskNumber
 
-# Resize the partition to the maximum size
 Resize-Partition -PartitionNumber $partition.PartitionNumber -DiskNumber $partition.DiskNumber -Size $supportedSize.SizeMax
 
 Write-Output "Disk $DriveLetter extended to maximum size."
